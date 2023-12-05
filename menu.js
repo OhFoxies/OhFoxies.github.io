87e95dc20c52f085cc7be2f9f5562abf3e29294d
OhFoxies <w.rejner@gmail.com>
2023-12-05 22:44:55 +0100

    d

--- menu.js
+++ menu.js
@@ -1,19 +1,35 @@
-function getCalendar() {
-    let date = new Date();
-    
-    let days = ['Poniedzialek', "Wtorek", "Sroda", "Czwartek", "Piatek", "Sobota", "Niedziela"]
-    let months = ["Styczen", "Luty", "Marzec", "Kwiecień", "Maj", "Czerwiec", "Lipiec", "Sierpien", "Wrzesien", "Październik", "Listopad", "Grudzien"]
-    let month = date.getMonth()
-    let day = date.getDate()
-    let year = date.getFullYear()
-    let day_name = date.getDay()
-    let date_container = document.getElementById("date")
-    let month_container = document.getElementById("month")
-    let year_container = document.getElementById("year")
-    let day_container = document.getElementById("day")
-    date_container.innerHTML = "" + day;
-    month_container.innerHTML = months[month]
-    year_container.innerHTML = "" + year
-    day_container.innerHTML = days[day_name - 1]
-    console.log("Miesiąc: " + month + " day: " + day + " year: " + year)
+let menu_button = document.getElementById("more-btn")
+menu_button.addEventListener("click", function () {
+    dropdownMenu()
+})
+// menu_button.addEventListener("mouseover", function () {dropdownMenu()})
+document.addEventListener("scroll", function () {
+    pageScroll()
+
+})
+
+function pageScroll() {
+    let menu = document.getElementById("menu-content");
+    if (getComputedStyle(menu).display !== 'none') {
+        menu.style.display = 'none'
+    }
+
+}
+
+function dropdownMenu() {
+    let menu = document.getElementById("menu-content");
+    if (getComputedStyle(menu).display === 'none') {
+        menu.style.display = 'flex';
+        console.log("test")
+    } else {
+        menu.style.display = 'none';
+        console.log("test2")
+
+    }
+
+}
+
+function menu() {
+    let menu = document.getElementById('menu')
+    menu.classList.toggle("open-menu")
 }
